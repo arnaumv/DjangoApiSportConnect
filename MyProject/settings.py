30c0205de3207045d3932684d54b0048cb3e43bb
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-03-19 12:47:01 +0100

    ...

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sportconnect',
         'USER': 'root',
-        'PASSWORD': 'P@ssw0rd',
+        'PASSWORD': 'root',
         'HOST': 'localhost',  
         'PORT': '3306',        
     }
@@ -136,3 +136,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5501",
+]
