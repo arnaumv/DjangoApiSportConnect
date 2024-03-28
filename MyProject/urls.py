c75f4db844930fb69c54dad8c21df4357fc538cb
Xavi Martinez <xaavii0711@gmail.com>
2024-03-28 18:47:26 +0100

    Ok unbirse/salir Eventos

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -1,30 +1,16 @@
-"""
-URL configuration for MyProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from myapp.views import UserViewSet, LoginView, UserProfileView,EventCreateViewSet, EventViewSet, UserIdView, join_event, get_participants
+from myapp.views import (
+    UserViewSet, LoginView, UserProfileView, EventCreateViewSet,
+    EventViewSet, UserIdView, join_event, get_participants, leave_event  # Importa la nueva vista leave_event
+)
 from myapp import views
 
 router = DefaultRouter()
 router.register(r'usuario', UserViewSet, basename='usuario')
 router.register(r'event', EventCreateViewSet, basename='event')  
 router.register(r'event-filter', EventViewSet, basename='event-filter') 
-
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -34,12 +20,14 @@ urlpatterns = [
     path('profile/<str:username>/', UserProfileView.as_view()),
     path('userid/<str:username>/', UserIdView.as_view(), name='user-id'),
     path('join-event/', join_event, name='join-event'),
-    path('event/<int:event_id>/participants', get_participants, name='get_participants'),
+    path('leave-event/', leave_event, name='leave-event'),  # Agrega la URL para leave_event
+    path('check-joined/', views.check_joined, name='check_joined'),
+    path('cancel-event/', views.cancel_event, name='cancel_event'),
 
+
+    path('event/<int:event_id>/participants', get_participants, name='get_participants'),
 
     path('reset_password/', views.reset_password, name='reset_password'),
     path('reset/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
-
     path('events/user_subscribed_events/', EventViewSet.as_view({'get': 'user_subscribed_events'}), name='user-subscribed-events'),
-
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import User
-from .serializers import UserSerializer, EventsJoinedSerializer
+from .models import User, Event, EventsJoined
+from .serializers import UserSerializer, EventSerializer, EventsJoined, EventsJoinedSerializer
 from django.contrib.auth.hashers import check_password
 from rest_framework import status, views
 from rest_framework.response import Response
@@ -10,11 +10,8 @@ from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, views
 from rest_framework.decorators import api_view
-from .models import EventsJoined
-
 
 # VIEW PARA CREAR USUARIO
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -36,22 +33,15 @@ class LoginView(views.APIView):
                 return Response({"error": "Invalid login credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         except User.DoesNotExist:
             return Response({"error": "Invalid login credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-        
 
 # VIEW PARA DEVOLVER INFORMACION DEL USAURIO (PROFILE.HTML)
-
 class UserProfileView(views.APIView):
-
     def get(self, request, username, *args, **kwargs):
         user = get_object_or_404(User, username=username)
-
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
-
-
-## VIEW QUE COMPRUEBA EL ID DEL USERANME PARA CREAR EL EVENTO  (CREATE.HTML)
-
+## VIEW QUE COMPRUEBA EL ID DEL USERANME PARA CREAR EL EVENTO (CREATE.HTML)
 class UserIdView(views.APIView):
     def get(self, request, username, *args, **kwargs):
         try:
@@ -60,16 +50,12 @@ class UserIdView(views.APIView):
         except User.DoesNotExist:
             return JsonResponse({'error': 'User not found'}, status=404)
 
-## VIEW CREAR EVENTO  (CREATE.HTML)
-from rest_framework import viewsets
-from .models import Event
-from .serializers import EventSerializer
-
+## VIEW CREAR EVENTO (CREATE.HTML)
 class EventCreateViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-## VIEW PARA MOSTRAR EVENTOS  (EVENTS.HTML)
+## VIEW PARA MOSTRAR EVENTOS (EVENTS.HTML)
 class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
 
@@ -88,14 +74,13 @@ class EventViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
         except Event.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
     @action(detail=False, methods=['get'])
     def user_subscribed_events(self, request):
         username = request.query_params.get('username')
         if username:
             user = get_object_or_404(User, username=username)
             try:
-                # Filtrar eventos por los que el usuario está suscrito
                 user_events = EventsJoined.objects.filter(user_id=user.id)
                 event_ids = user_events.values_list('event_id', flat=True)
                 subscribed_events = Event.objects.filter(id__in=event_ids)
@@ -106,8 +91,7 @@ class EventViewSet(viewsets.ModelViewSet):
         else:
             return Response({"detail": "Username parameter is missing."}, status=status.HTTP_400_BAD_REQUEST)
 
-## VIEW PARA UNIRSE A EVENTOS  (INFOEVENT.HTML)
-
+## VIEW PARA UNIRSE A EVENTOS (INFOEVENT.HTML)
 @api_view(['POST'])
 def join_event(request):
     username = request.data.get('username')
@@ -125,8 +109,73 @@ def join_event(request):
     serializer = EventsJoinedSerializer(events_joined)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+## VIEW PARA SALIR DE EVENTOS (INFOEVENT.HTML)
+@api_view(['POST'])
+def leave_event(request):
+    username = request.data.get('username')
+    event_id = request.data.get('event')
 
-## VIEW PARA MOSTRAR PARTICIPANTES DE UN EVENTO  (INFOEVENT.HTML)
+    try:
+        # Obtener la instancia de User
+        user = User.objects.get(username=username)
+        print('User found:', user.username, user.id)  # Mensaje para la consola del servidor
+    except User.DoesNotExist:
+        print('User not found with username:', username)  # Mensaje para la consola del servidor
+        return Response({"message": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        # Obtener la instancia de Event
+        event = Event.objects.get(pk=event_id)
+        print('Event found:', event.title, event.id)  # Mensaje para la consola del servidor
+    except Event.DoesNotExist:
+        print('Event not found with ID:', event_id)  # Mensaje para la consola del servidor
+        return Response({"message": "Evento no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        # Verificar si el usuario está unido al evento
+        event_joined = EventsJoined.objects.get(user_id_id=user.id, event_id=event.id)
+        print('EventsJoined found:', event_joined.id)  # Mensaje para la consola del servidor
+        # Eliminar la instancia de EventsJoined
+        event_joined.delete()
+        return Response({"message": "Usuario eliminado del evento correctamente"}, status=status.HTTP_204_NO_CONTENT)
+    except EventsJoined.DoesNotExist:
+        print('EventsJoined not found for user:', user.username, 'and event:', event.title)  # Mensaje para la consola del servidor
+        return Response({"message": "El usuario no está unido a este evento"}, status=status.HTTP_404_NOT_FOUND)
+    
+## VIEW PARA COMPROVAR SI EL USUARIO YA ESTA UNIDO AL EVENTO
+@api_view(['POST'])
+def check_joined(request):
+    username = request.data.get('username')
+    event_id = request.data.get('event')
+
+    try:
+        user_joined = EventsJoined.objects.filter(username=username, event_id=event_id).exists()
+    except EventsJoined.DoesNotExist:
+        user_joined = False
+
+    return Response({'joined': user_joined})
+
+## VIEW PARA SALIR DE LA MLISTA DE PARTICIPANTES DE UN EVENTO
+@api_view(['POST'])
+def cancel_event(request):
+    username = request.data.get('username')
+    event_id = request.data.get('event')
+
+    try:
+        user = User.objects.get(username=username)
+        event = Event.objects.get(pk=event_id)
+    except (User.DoesNotExist, Event.DoesNotExist):
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        event_joined = EventsJoined.objects.get(user_id=user.id, event_id=event.id)
+        event_joined.delete()
+        return Response({"message": "Usuario eliminado del evento correctamente"}, status=status.HTTP_204_NO_CONTENT)
+    except EventsJoined.DoesNotExist:
+        return Response({"message": "El usuario no está unido a este evento"}, status=status.HTTP_404_NOT_FOUND)
+
+
+## VIEW PARA MOSTRAR PARTICIPANTES DE UN EVENTO (INFOEVENT.HTML)
 @api_view(['GET'])
 def get_participants(request, event_id):
     try:
@@ -135,18 +184,6 @@ def get_participants(request, event_id):
         return Response(serializer.data)
     except EventsJoined.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-
-
-
-
-
-
-
-
-
-
 
 ## VIEW PARA RESTABLECER CONTRASEÑA
 from django.contrib.auth.forms import PasswordResetForm
