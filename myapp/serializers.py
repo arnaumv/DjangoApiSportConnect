c925aeeb3397d7c7c0bdc61a786907a305ebd466
arnaumv <arnaumestre02@gmail.com>
2024-04-15 21:08:33 +0200

    IMPLEMENTANDO AÑADIR IMAGEN PERFIL

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -25,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'city', 'birthdate', 'description'] 
+        fields = ['username', 'email', 'password', 'city', 'birthdate', 'description', "image_path"]
         extra_kwargs = {
             'password': {'write_only': True}
         }
